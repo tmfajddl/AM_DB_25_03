9db3a8d830ba835dbf76ddcf79a24c697f8f1031
tmfajddl <tmfajddl12@gmail.com>
2025-03-31 11:25:07 +0900

    수정,삭제,상세보기 추가

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,8 +1,12 @@
 package org.example;
 
+import org.example.util.DBUtil;
+import org.example.util.SecSql;
+
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.Scanner;
 
 public class App {
@@ -16,7 +20,7 @@ public class App {
             String cmd = sc.nextLine().trim();
 
             Connection conn = null;
-            //계속 주기적으로 연결되었는지 확인해 줌으로써 실행이 중지 되지 않도록 만들어줌
+
             try {
                 Class.forName("org.mariadb.jdbc.Driver");
             } catch (ClassNotFoundException e) {
@@ -63,85 +67,35 @@ public class App {
             System.out.print("내용 : ");
             String body = sc.nextLine();
 
-            PreparedStatement pstmt = null;
+            SecSql sql = new SecSql();
 
-            try {
-                String sql = "INSERT INTO article ";
-                sql += "SET regDate = NOW(),";
-                sql += "updateDate = NOW(),";
-                sql += "title = '" + title + "',";
-                sql += "`body`= '" + body + "';";
+            sql.append("INSERT INTO article");
+            sql.append("SET regDate = NOW(),");
+            sql.append("updateDate = NOW(),");
+            sql.append("title = ?,", title);
+            sql.append("`body` = ?;", body);
 
-                System.out.println(sql);
+            int id = DBUtil.insert(conn, sql);
 
-                pstmt = conn.prepareStatement(sql);
+            System.out.println(id + "번 글이 생성됨");
 
-                int affectedRow = pstmt.executeUpdate();
-
-                System.out.println(affectedRow + "열에 적용됨");
-
-            } catch (SQLException e) {
-                System.out.println("에러 2: " + e);
-            } finally {
-                try {
-                    if (pstmt != null && !pstmt.isClosed()) {
-                        pstmt.close();
-                    }
-                } catch (SQLException e) {
-                    e.printStackTrace();
-                }
-            }
 
         } else if (cmd.equals("article list")) {
             System.out.println("==목록==");
 
-            PreparedStatement pstmt = null;
-            ResultSet rs = null;
-
             List<Article> articles = new ArrayList<>();
 
-            try {
-                String sql = "SELECT *";
-                sql += " FROM article";
-                sql += " ORDER BY id DESC;";
+            SecSql sql = new SecSql();
+            sql.append("SELECT *");
+            sql.append("FROM article");
+            sql.append("ORDER BY id DESC");
 
-                System.out.println(sql);
+            List<Map<String, Object>> articleListMap = DBUtil.selectRows(conn, sql);
 
-                pstmt = conn.prepareStatement(sql);
-
-                rs = pstmt.executeQuery(sql);
-
-                while (rs.next()) {
-                    int id = rs.getInt("id");
-                    String regDate = rs.getString("regDate");
-                    String updateDate = rs.getString("updateDate");
-                    String title = rs.getString("title");
-                    String body = rs.getString("body");
-
-                    Article article = new Article(id, regDate, updateDate, title, body);
-
-                    articles.add(article);
-                }
-
-            } catch (SQLException e) {
-                System.out.println("에러 3 : " + e);
-            } finally {
-                try {
-                    if (rs != null && !rs.isClosed()) {
-                        rs.close();
-                    }
-                } catch (SQLException e) {
-                    e.printStackTrace();
-                }
-                try {
-                    if (pstmt != null && !pstmt.isClosed()) {
-                        pstmt.close();
-                    }
-                } catch (SQLException e) {
-                    e.printStackTrace();
-                }
-
+            for (Map<String, Object> articleMap : articleListMap) {
+                articles.add(new Article(articleMap));
             }
+
             if (articles.size() == 0) {
                 System.out.println("게시글이 없습니다");
                 return 0;
@@ -162,44 +116,85 @@ public class App {
                 return 0;
             }
 
+
             System.out.println("==수정==");
             System.out.print("새 제목 : ");
             String title = sc.nextLine().trim();
             System.out.print("새 내용 : ");
             String body = sc.nextLine().trim();
 
-            PreparedStatement pstmt = null;
+            SecSql sql = new SecSql();
+            sql.append("UPDATE article");
+            sql.append("SET updateDate = NOW()");
+
+            if (title.length() > 0) {
+                sql.append(",title = ?", title);
+            }
+            if (body.length() > 0) {
+                sql.append(",`body` = ?", body);
+            }
+            sql.append("WHERE id = ?;",id);
+
+            int rows = DBUtil.update(conn, sql);
+            if (rows == 0) {
+                System.out.println("해당 게시물이 없습니다.");
+                return 0;
+            }
+
+            System.out.println(id + "번 글이 수정되었습니다.");
+        }
+        else if (cmd.startsWith("article detail")) {
+            System.out.println("==상세보기==");
+            int id = 0;
 
             try {
-                String sql = "UPDATE article";
-                sql += " SET updateDate = NOW()";
-                if (title.length() > 0) {
-                    sql += " ,title = '" + title + "'";
-                }
-                if (body.length() > 0) {
-                    sql += " ,`body` = '" + body + "'";
-                }
-                sql += " WHERE id = " + id + ";";
-
-                System.out.println(sql);
-
-                pstmt = conn.prepareStatement(sql);
-
-                pstmt.executeUpdate();
-
-            } catch (SQLException e) {
-                System.out.println("에러 4 : " + e);
-            } finally {
-                try {
-                    if (pstmt != null && !pstmt.isClosed()) {
-                        pstmt.close();
-                    }
-                } catch (SQLException e) {
-                    e.printStackTrace();
-                }
-
+                id = Integer.parseInt(cmd.split(" ")[2]);
+            } catch (Exception e) {
+                System.out.println("번호는 정수로 입력해");
+                return 0;
             }
-            System.out.println(id + "번 글이 수정되었습니다.");
+
+            SecSql sql = new SecSql();
+            sql.append("SELECT *");
+            sql.append("FROM article");
+            sql.append("WHERE id = ?;",id);
+            List<Map<String, Object>> articleListMap = DBUtil.selectRows(conn, sql);
+            if(articleListMap.size() == 0) {
+                System.out.println("해당 게시물이 없습니다.");
+                return 0;
+            }
+            for (Map<String, Object> articleMap : articleListMap) {
+                if(articleMap.get("id").equals(id)) {
+                    System.out.println("id : " + articleMap.get("id"));
+                    System.out.println("regDate : " + articleMap.get("regDate"));
+                    System.out.println("updateDate : " + articleMap.get("updateDate"));
+                    System.out.println("title : " + articleMap.get("title"));
+                    System.out.println("body : " + articleMap.get("body"));
+                    break;
+                }
+            }
+
+        }
+        else if (cmd.startsWith("article delete")) {
+            int id = 0;
+            try {
+                id = Integer.parseInt(cmd.split(" ")[2]);
+            } catch (Exception e) {
+                System.out.println("번호는 정수로 입력해");
+                return 0;
+            }
+
+            SecSql sql = new SecSql();
+            sql.append("DELETE FROM article");
+            sql.append("WHERE id = ?;",id);
+            int rows = DBUtil.delete(conn, sql);
+
+            if (rows == 0) {
+                System.out.println("해당 게시물이 없습니다.");
+                return 0;
+            }
+
+            System.out.println(id + "번 글이 삭제되었습니다.");
         }
         return 0;
     }
--- src/main/java/org/example/Article.java
+++ src/main/java/org/example/Article.java
@@ -1,11 +1,21 @@
 package org.example;
 
+import java.util.Map;
+
 public class Article {
     private int id;
-    private String title;
-    private String body;
     private String regDate;
     private String updateDate;
+    private String title;
+    private String body;
+
+    public Article(int id, String regDate, String updateDate, String title, String body) {
+        this.id = id;
+        this.regDate = regDate;
+        this.updateDate = updateDate;
+        this.title = title;
+        this.body = body;
+    }
 
     public Article(int id, String title, String body) {
         this.id = id;
@@ -13,12 +23,12 @@ public class Article {
         this.body = body;
     }
 
-    public Article(int id, String regDate, String updateDate, String title, String body) {
-        this.id = id;
-        this.title = title;
-        this.body = body;
-        this.regDate = regDate;
-        this.updateDate = updateDate;
+    public Article(Map<String, Object> articleMap) {
+        this.id = (int) articleMap.get("id");
+        this.regDate = (String) articleMap.get("regDate");
+        this.updateDate = (String) articleMap.get("updateDate");
+        this.title = (String) articleMap.get("title");
+        this.body = (String) articleMap.get("body");
     }
 
     @Override
@@ -29,6 +39,23 @@ public class Article {
                 ", body='" + body + '\'' +
                 '}';
     }
+
+    public String getRegDate() {
+        return regDate;
+    }
+
+    public void setRegDate(String regDate) {
+        this.regDate = regDate;
+    }
+
+    public String getUpdateDate() {
+        return updateDate;
+    }
+
+    public void setUpdateDate(String updateDate) {
+        this.updateDate = updateDate;
+    }
+
 
     public int getId() {
         return id;
@@ -52,17 +79,5 @@ public class Article {
 
     public void setBody(String body) {
         this.body = body;
-    }
-    public String getRegDate() {
-        return regDate;
-    }
-    public void setRegDate(String regDate) {
-        this.regDate = regDate;
-    }
-    public String getUpdateDate() {
-        return updateDate;
-    }
-    public void setUpdateDate(String updateDate) {
-        this.updateDate = updateDate;
     }
 }
